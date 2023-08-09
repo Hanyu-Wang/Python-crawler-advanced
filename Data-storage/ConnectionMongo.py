c3920e08b2544af5241c87fbe304d1b9c24145a2
HanyuWang <wanghanyu1108@outlook.com>
2023-08-09 11:18:16 +0800

    add Env_var

--- Data-storage/ConnectionMongo.py
+++ Data-storage/ConnectionMongo.py
@@ -1,11 +1,27 @@
-from pymongo import MongoClient
-from pyhocon import ConfigFactory
+import os
+import httpx
+import pymongo
 
-conf = ConfigFactory.parse_file("database.conf")
-host = conf.get("database.mongo.host")
-port = conf.get("database.mongo.port")
-username = conf["databases"]["mongo"]["username"]
-password = conf.get_config("databases")["mongo.password"]
-client = MongoClient(host, port)
-db_auth = client.whytest
-db_auth.authenticate(username, password)
+
+mongo_con = os.getenv('MONGO_CONNECTION')
+client = pymongo.MongoClient(mongo_con)
+"""
+pymongo升级到4.0后的认证方式更改 https://pymongo.readthedocs.io/en/stable/migrate-to-pymongo4.html#database-authenticate-and-database-logout-are-removed
+"""
+db = client.whytest
+collection = db["students"]
+
+
+def get_data():
+    res = httpx.get(
+        "https://careers.tencent.com/tencentcareer/api/post/Query?timestamp=1691550385360&countryId=&cityId=&bgIds=&productId=&categoryId=&parentCategoryId=&attrId=&keyword=%25E6%25B5%258B%25E8%25AF%2595&pageIndex=1&pageSize=10&language=zh-cn&area=cn"
+    )
+    items = res.json()
+    item = items.get("Data")["Posts"]
+    for i in item[0:1]:
+        if isinstance(i, dict):
+            collection.insert_one(i)
+
+
+if __name__ == "__main__":
+    get_data()
---
+++ Data-storage/Env_Var.py
@@ -0,0 +1,4 @@
+import os
+# Set environment variables，Env Var Names Must Be UPPERCASE
+os.environ['MONGO_CONNECTION'] = 'mongodb://root:123456@10.0.200.169:27017'
+
